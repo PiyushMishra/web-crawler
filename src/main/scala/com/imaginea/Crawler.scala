75a0cf861e800f893edf96fb37d5d38e75254765
Piyush Mishra <piyushmishra889@gmail.com>
2015-02-03 15:52:50 +0530

    filter contents

--- src/main/scala/com/imaginea/Crawler.scala
+++ src/main/scala/com/imaginea/Crawler.scala
@@ -10,6 +10,7 @@ import edu.uci.ics.crawler4j.fetcher.PageFetcher
 import edu.uci.ics.crawler4j.robotstxt.RobotstxtServer
 import edu.uci.ics.crawler4j.crawler.CrawlController
 import edu.uci.ics.crawler4j.robotstxt.RobotstxtConfig
+import scala.collection.JavaConversions._
 
 class Crawler extends WebCrawler {
 
@@ -18,23 +19,21 @@ class Crawler extends WebCrawler {
     + "|wav|avi|mov|mpeg|ram|m4v|pdf"
     + "|rm|smil|wmv|swf|wma|zip|rar|gz))$")
 
-  @Override def shouldVisitPage(url: WebURL): Boolean = {
+  override def shouldVisit(page: Page, url: WebURL): Boolean = {
     val href = url.getURL().toLowerCase()
-    !contentTobefilterd.matcher(href).matches() && href.startsWith("http://www.ics.uci.edu/");
+    !contentTobefilterd.matcher(href).matches() && href.startsWith("http://mail-archives.apache.org/mod_mbox/maven-users/2014")
   }
 
   override def visit(page: Page) {
     val url = page.getWebURL().getURL()
     println("URL: " + url)
-
     page.getParseData match {
-
       case htmlParseData: HtmlParseData =>
         val text = htmlParseData.getText
         val html = htmlParseData.getHtml()
         val links = htmlParseData.getOutgoingUrls()
-        println("Text length: " + text.length())
-        println("Html length: " + html.length())
+//        println(links.toSet.filter { url => url.getURL.contains("2014") && url.getURL.contains("mbox/browser") })
+        println("Text : " + text)
         println("Number of outgoing links: " + links.size())
 
       case _ => // do nothing
@@ -56,9 +55,7 @@ object CrawlerApp extends App {
   val robotstxtServer = new RobotstxtServer(robotstxtConfig, pageFetcher)
   val controller = new CrawlController(config, pageFetcher, robotstxtServer)
 
-  controller.addSeed("http://www.ics.uci.edu/~welling/")
-  controller.addSeed("http://www.ics.uci.edu/~lopes/")
-  controller.addSeed("http://www.ics.uci.edu/")
+  controller.addSeed("http://mail-archives.apache.org/mod_mbox/maven-users/")
 
   controller.start(classOf[Crawler], numberOfCrawlers)
 
